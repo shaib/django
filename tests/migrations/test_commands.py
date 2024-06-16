0e8e5bc57adf98bf28e77fa49bdc1cfb4ce949eb
Clinton Christian <pygeek@users.noreply.github.com>
2024-06-16 18:54:03 -0400

    Fixed #35508: Complete business logic for filtering out relevant field and model operations.

--- tests/migrations/test_commands.py
+++ tests/migrations/test_commands.py
@@ -3477,7 +3477,8 @@ class SquashMigrationsTests(MigrationTestBase):
     )
     def test_squashmigrations_ignore_dependencies_alter_relation_multi_apps(self):
         with self.temporary_migration_module(
-                module="migrations.migrations_test_apps.alter_fk.book_app"):
+            module="migrations.migrations_test_apps.alter_fk.book_app"
+        ):
             call_command(
                 "squashmigrations",
                 "book_app",
@@ -3487,6 +3488,8 @@ class SquashMigrationsTests(MigrationTestBase):
                 ignore_dependencies=True,
                 verbosity=0,
             )
+
+        call_command("migrate", "book_app", "zero", verbosity=0)
 
 
 class AppLabelErrorTests(TestCase):
